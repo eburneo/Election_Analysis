79c34b9108226862e8c2439973e1dc9076de768b
Art <eburneotx@gmail.com>
2021-12-11 21:01:26 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -33,7 +33,7 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data) 
 
     # Read the header row
-    header = next(file_reader)  
+    headers = next(file_reader)                              
 
     # Print each row in the CSV file.
     for row in file_reader:
@@ -54,39 +54,68 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-#Determie the percentage of votes for each Candidate
-# Iterate through the candidate list
-for candidate_name in candidate_votes:
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    election_results = ( 
+        f"\nElection Results\n"
+        f"--------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"--------------------\n")
+    print(election_results, end="")
 
-    # Retreive vote count of a candidate
-    votes = candidate_votes[candidate_name]
+    # Save the final vote count to the text file
+    txt_file.write(election_results) 
 
-    # calculate the percentage of votes
-    vote_percentage = float(votes)/float(total_votes) * 100
 
-    # To do print out each candidate's name, vote count and percentage of
-    #votes to the terminal
 
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning vote count and candidate
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # If true then set winning_count = votes  and winning_percent = vote percentage
-        winning_count = votes
-        winning_percentage =  vote_percentage
-        # Set the winning_Candidate equal to the candidate's name.
-        winning_candidate = candidate_name 
+    #Determie the percentage of votes for each Candidate
+    # Iterate through the candidate list
+    for candidate_name in candidate_votes:
 
-# To do: print out the winning candidate, vote count and percentage to
-#Terminal
-winning_candidate_summary = (
-    f"----------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"----------------------\n")
-print(winning_candidate_summary)
+        # Retreive vote count of a candidate
+        votes = candidate_votes[candidate_name]
+
+        # calculate the percentage of votes
+        vote_percentage = float(votes)/float(total_votes) * 100
+
+        # To do print out each candidate's name, vote count and percentage of
+        #votes to the terminal
+
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Print each candidate's voter count and percentage to the terminal
+        print(candidate_results)
+
+        # Save the candidate results to the terminal
+        txt_file.write(candidate_results) 
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes  and winning_percent = vote percentage
+            winning_count = votes
+            winning_percentage =  vote_percentage
+            # Set the winning_Candidate equal to the candidate's name.
+            winning_candidate = candidate_name 
+
+    # To do: print out the winning candidate, vote count and percentage to
+    #Terminal
+    winning_candidate_summary = (
+        f"----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------------\n")
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's result to the text file
+    txt_file.write(winning_candidate_summary) 
+
+
+
+
 
 
 
